41a3298cc4109be290204656f025726f74c9f19c
VedanshSaha <76149219+VedanshSaha@users.noreply.github.com>
2022-02-28 16:56:27 +0530

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
 df = pd.read_csv('dwarf_stars.csv')
-# df = df['Star_name'].notna()
-df = df['Distance'].notna()
+df = df[df['Distance'].notna()]
+df = df[df['Mass'].notna()]
+df = df[df['Radius'].notna()]
 
-for i in enumerate(df]) :
-    a = df[4][i]
-    print(a)
+df["Mass"] = 0.000954588*df["Mass"]
+df["Radius"] = 0.102763*df["Radius"]
+
